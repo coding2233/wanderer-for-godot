43f24924fc5416f35b4c6b26b9e26883d4e8b60b
wanderer <dutifulwanderer@gmail.com>
2021-05-23 17:28:18 +0800

    godot nativescript

---
+++ src/wanderer.c
@@ -0,0 +1,95 @@
+#include <string.h>
+
+
+#include "gdnative_api_struct.gen.h"
+
+typedef struct user_data_struct {
+	char data[256];
+} user_data_struct;
+
+const godot_gdnative_core_api_struct *api = NULL;
+const godot_gdnative_ext_nativescript_api_struct *nativescript_api = NULL;
+
+void *wanderer_constructor(godot_object *p_instance, void *p_method_data);
+
+void wanderer_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
+
+godot_variant wanderer_get_data(godot_object *p_instance, void *p_method_data,
+        void *p_user_data, int p_num_args, godot_variant **p_args);
+
+
+void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options) {
+    api = p_options->api_struct;
+
+    // Now find our extensions.
+    for (int i = 0; i < api->num_extensions; i++) {
+        switch (api->extensions[i]->type) {
+            case GDNATIVE_EXT_NATIVESCRIPT: {
+                nativescript_api = (godot_gdnative_ext_nativescript_api_struct *)api->extensions[i];
+            }; break;
+            default: break;
+        }
+    }
+}
+
+
+void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *p_options) {
+    api = NULL;
+    nativescript_api = NULL;
+}
+
+
+void GDN_EXPORT godot_nativescript_init(void *p_handle) {
+    godot_instance_create_func create = { NULL, NULL, NULL };
+    create.create_func = &wanderer_constructor;
+
+    godot_instance_destroy_func destroy = { NULL, NULL, NULL };
+    destroy.destroy_func = &wanderer_destructor;
+
+    nativescript_api->godot_nativescript_register_class(p_handle, "wanderer", "Reference",
+            create, destroy);
+
+    godot_instance_method get_data = { NULL, NULL, NULL };
+    get_data.method = &wanderer_get_data;
+
+    godot_method_attributes attributes = { GODOT_METHOD_RPC_MODE_DISABLED };
+
+    nativescript_api->godot_nativescript_register_method(p_handle, "wanderer", "get_data",
+            attributes, get_data);
+}
+
+
+// In our constructor, allocate memory for our structure and fill
+// it with some data. Note that we use Godot's memory functions
+// so the memory gets tracked and then return the pointer to
+// our new structure. This pointer will act as our instance
+// identifier in case multiple objects are instantiated.
+GDCALLINGCONV void *wanderer_constructor(godot_object *p_instance, void *p_method_data) {
+	user_data_struct *user_data = api->godot_alloc(sizeof(user_data_struct));
+	strcpy(user_data->data, "World from GDNative!");
+
+	return user_data;
+}
+
+// The destructor is called when Godot is done with our
+// object and we free our instances' member data.
+GDCALLINGCONV void wanderer_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data) {
+	api->godot_free(p_user_data);
+}
+
+// Data is always sent and returned as variants so in order to
+// return our data, which is a string, we first need to convert
+// our C string to a Godot string object, and then copy that
+// string object into the variant we are returning.
+godot_variant wanderer_get_data(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
+	godot_string data;
+	godot_variant ret;
+	user_data_struct *user_data = (user_data_struct *)p_user_data;
+
+	api->godot_string_new(&data);
+	api->godot_string_parse_utf8(&data, user_data->data);
+	api->godot_variant_new_string(&ret, &data);
+	api->godot_string_destroy(&data);
+
+	return ret;
+}
