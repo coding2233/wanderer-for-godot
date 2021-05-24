a97102d079a705920ea608fd65b0f29e49b671d8
wanderer <dutifulwanderer@gmail.com>
2021-05-24 07:07:51 +0000

    Github actions

--- src/wanderer.c
+++ src/wanderer.c
@@ -1,8 +1,11 @@
 #include <string.h>
+
+// #include "export.h"
 #include "gdnative_api_struct.gen.h"
 
-typedef struct user_data_struct {
-	char data[256];
+typedef struct user_data_struct
+{
+    char data[256];
 } user_data_struct;
 
 const godot_gdnative_core_api_struct *api = NULL;
@@ -13,81 +16,101 @@ void *wanderer_constructor(godot_object *p_instance, void *p_method_data);
 void wanderer_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
 
 godot_variant wanderer_get_data(godot_object *p_instance, void *p_method_data,
-        void *p_user_data, int p_num_args, godot_variant **p_args);
+                                void *p_user_data, int p_num_args, godot_variant **p_args);
 
+godot_variant wanderer_connect(godot_object *p_instance, void *p_method_data,
+                               void *p_user_data, int p_num_args, godot_variant **p_args);
 
-void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options) {
+void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options)
+{
     api = p_options->api_struct;
 
     // Now find our extensions.
-    for (int i = 0; i < api->num_extensions; i++) {
-        switch (api->extensions[i]->type) {
-            case GDNATIVE_EXT_NATIVESCRIPT: {
-                nativescript_api = (godot_gdnative_ext_nativescript_api_struct *)api->extensions[i];
-            }; break;
-            default: break;
+    for (int i = 0; i < api->num_extensions; i++)
+    {
+        switch (api->extensions[i]->type)
+        {
+        case GDNATIVE_EXT_NATIVESCRIPT:
+        {
+            nativescript_api = (godot_gdnative_ext_nativescript_api_struct *)api->extensions[i];
+        };
+        break;
+        default:
+            break;
         }
     }
 }
 
-
-void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *p_options) {
+void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *p_options)
+{
     api = NULL;
     nativescript_api = NULL;
 }
 
-
-void GDN_EXPORT godot_nativescript_init(void *p_handle) {
-    godot_instance_create_func create = { NULL, NULL, NULL };
+void GDN_EXPORT godot_nativescript_init(void *p_handle)
+{
+    godot_instance_create_func create = {NULL, NULL, NULL};
     create.create_func = &wanderer_constructor;
 
-    godot_instance_destroy_func destroy = { NULL, NULL, NULL };
+    godot_instance_destroy_func destroy = {NULL, NULL, NULL};
     destroy.destroy_func = &wanderer_destructor;
 
     nativescript_api->godot_nativescript_register_class(p_handle, "wanderer", "Reference",
-            create, destroy);
+                                                        create, destroy);
 
-    godot_instance_method get_data = { NULL, NULL, NULL };
+    godot_instance_method get_data = {NULL, NULL, NULL};
     get_data.method = &wanderer_get_data;
 
-    godot_method_attributes attributes = { GODOT_METHOD_RPC_MODE_DISABLED };
+    godot_method_attributes attributes = {GODOT_METHOD_RPC_MODE_DISABLED};
 
     nativescript_api->godot_nativescript_register_method(p_handle, "wanderer", "get_data",
-            attributes, get_data);
-}
+                                                         attributes, get_data);
 
+    godot_instance_method connect = {NULL, NULL, NULL};
+    connect.method = &wanderer_connect;
+    nativescript_api->godot_nativescript_register_method(p_handle, "wanderer", "connect", attributes, connect);
+}
 
 // In our constructor, allocate memory for our structure and fill
 // it with some data. Note that we use Godot's memory functions
 // so the memory gets tracked and then return the pointer to
 // our new structure. This pointer will act as our instance
 // identifier in case multiple objects are instantiated.
-GDCALLINGCONV void *wanderer_constructor(godot_object *p_instance, void *p_method_data) {
-	user_data_struct *user_data = api->godot_alloc(sizeof(user_data_struct));
-	strcpy(user_data->data, "World from GDNative!");
+GDCALLINGCONV void *wanderer_constructor(godot_object *p_instance, void *p_method_data)
+{
+    user_data_struct *user_data = api->godot_alloc(sizeof(user_data_struct));
+    strcpy(user_data->data, "World from GDNative!");
 
-	return user_data;
+    return user_data;
 }
 
 // The destructor is called when Godot is done with our
 // object and we free our instances' member data.
-GDCALLINGCONV void wanderer_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data) {
-	api->godot_free(p_user_data);
+GDCALLINGCONV void wanderer_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data)
+{
+    api->godot_free(p_user_data);
 }
 
 // Data is always sent and returned as variants so in order to
 // return our data, which is a string, we first need to convert
 // our C string to a Godot string object, and then copy that
 // string object into the variant we are returning.
-godot_variant wanderer_get_data(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_string data;
-	godot_variant ret;
-	user_data_struct *user_data = (user_data_struct *)p_user_data;
+godot_variant wanderer_get_data(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args)
+{
+    godot_string data;
+    godot_variant ret;
+    user_data_struct *user_data = (user_data_struct *)p_user_data;
 
-	api->godot_string_new(&data);
-	api->godot_string_parse_utf8(&data, user_data->data);
-	api->godot_variant_new_string(&ret, &data);
-	api->godot_string_destroy(&data);
+    api->godot_string_new(&data);
+    api->godot_string_parse_utf8(&data, user_data->data);
+    api->godot_variant_new_string(&ret, &data);
+    api->godot_string_destroy(&data);
 
-	return ret;
+    return ret;
+}
+
+godot_variant wanderer_connect(godot_object *p_instance, void *p_method_data,
+                               void *p_user_data, int p_num_args, godot_variant **p_args)
+{
+    // Connect("127.0.0.1",12233,)
 }
